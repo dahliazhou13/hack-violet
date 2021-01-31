56a968607a9ec6cc13f6e11b31edaa203b752de1
unknown <ananioluwatobiloba2000@gmail.com>
2021-01-31 02:07:24 +0100

    created nav component

---
+++ src/component/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css';
+
+export default function Nav() {
+    return (
+        <div className="nav_container">
+            <div className="nav_home">
+               <svg width="30px" className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+            </div>
+            <div className="nav_user">
+               <svg width="30px" className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            </div>
+        </div>
+    )
+}
